94edc7f55582f6788223c1e4f5234d9a84a5e48b
Admin <Admin@ADMIN-PC>
2018-09-26 23:09:25 +0400

    Add

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // bool
+            bool name = false;
+            name = true;
+            Console.WriteLine(name);
+            if (true)
+                Console.WriteLine("true");
+
+            else
+                Console.WriteLine(true);
+
+
+            //float
+            float f5 = 2.4f;
+            float f6 = 3.4f;
+
+            float Result = f5 * f6;
+            Console.WriteLine(f5 * f6);
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
